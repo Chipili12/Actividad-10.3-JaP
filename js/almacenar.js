51a733bc54a51d06fd824bb96991c5009544fd6e
Chipili12 <santibur@live.com>
2024-09-05 10:34:47 -0300

    Simplifications

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,8 +1,8 @@
 // Agrega los items de local storage al cargar
-document.addEventListener("DOMContentLoaded", function () {
+let donde = document.getElementById("contenedor");
+let nuevoJ = JSON.parse(localStorage.getItem("local")) || [];
 
-    let donde = document.getElementById("contenedor");
-    let nuevoJ = JSON.parse(localStorage.getItem("local")) || [];
+document.addEventListener("DOMContentLoaded", function () {
 
     nuevoJ.forEach(nuevo => {
         const li = document.createElement("li");
@@ -16,33 +16,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Boton agregar
 document.getElementById("agregar").addEventListener("click", function () {
-
     let nuevo = document.getElementById("item").value; // item nuevo
-    let donde = document.getElementById("contenedor");
-    let nuevoJ = JSON.parse(localStorage.getItem("local")) || [];
 
     if (nuevo !== "") {
-        nuevoJ.push(nuevo)
-        const li = document.createElement("li")
-        li.classList.add("list-group-item")
-        li.textContent = nuevo;
-        donde.appendChild(li);
-        localStorage.setItem("local", JSON.stringify(nuevoJ))
+        nuevoJ.push(nuevo),
+            li = document.createElement("li"),
+            li.classList.add("list-group-item"),
+            li.textContent = nuevo,
+            donde.appendChild(li),
+            localStorage.setItem("local", JSON.stringify(nuevoJ))
     }
-    else {
-        alert("Ingresa un item");
-    }
+    else alert("Ingresa un item");
+
 
     document.getElementById("item").value = "";
 
 })
 
-
-
 //Botón borrar
 document.getElementById("limpiar").addEventListener("click", function () {
 
-    document.getElementById("contenedor").innerHTML = "";
+    donde.innerHTML = "";
     localStorage.removeItem("local")
 
 })
